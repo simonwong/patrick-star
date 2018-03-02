6d364f4797d5c7cca58aea4144ac14e5ed2c79c3
wsj <wsj_simon@163.com>
2018-03-02 09:27:14 +0800

    project init

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path')
+
+const ROOT_PATH = path.resolve(__dirname)
+const ENTRY_PATH = path.resolve(ROOT_PATH, 'app')
+const OUTPUT_PATH = path.resolve(ROOT_PATH, 'build')
+const DEVSERVER_path = path.resolve(ROOT_PATH, 'public')
+
+module.exports = {
+    entry: {
+        index: [
+            'babel-polyfill',
+            path.resolve(ENTRY_PATH, 'index.js')
+        ],
+        vendor: ['react']
+    },
+    output: {
+        path: OUTPUT_PATH,
+        publicPath: '/',
+        // filename: '[name]-[hash:8].js',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: DEVSERVER_path,
+        historyApiFallback: true,
+        inline: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|jpg|gif|JPG|GIF|PNG|BMP|bmp|JPEG|jpeg)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(eot|woff|ttf|woff2|svg)$/,
+                use: 'url-loader'
+            }
+        ]
+    },
+    mode: 'development'
+}
